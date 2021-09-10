a8a1ffd5e0f4e34157b3b488ee91aac91fc07897
Christine Contreras <christineec23@yahoo.com>
2021-09-09 21:13:17 -0500

    working

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,3 +1,29 @@
 class Cohort < ActiveRecord::Base
-  
+  has_many :students
+
+  def add_student(name, age)
+    student = Student.find_or_create_by(name: name, age: age)
+    self.students << student
+    student
+  end
+
+  def average_age
+    (self.students.map {|student| student.age }.sum / self.students.count).to_f
+  end
+
+  def total_students
+    (self.students.count).to_i
+  end
+
+  def self.biggest
+    self.all.sort_by do |cohort|
+        cohort.students.count
+    end.reverse.first
+  end
+
+  def self.sort_by_mod
+    self.order(current_mod: :asc)
+  end
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,28 +5,57 @@ Student.destroy_all
 # This will create 5 cohorts using a random date
 puts "Seeding cohorts..."
 c1 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
+  name: "nyc-dumbo-web-2020",
   current_mod: rand(1..5)
 )
 c2 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
+  name: "nyc-dumbo-web-2021",
   current_mod: rand(1..5)
 )
 c3 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
+  name: "nyc-dumbo-web-2019",
   current_mod: rand(1..5)
 )
 c4 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
+  name: "nyc-dumbo-web-2020",
   current_mod: rand(1..5)
 )
 c5 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
+  name: "nyc-dumbo-web-2017",
   current_mod: rand(1..5)
 )
 
 puts "Seeding students..."
 # TODO: create seed data for students
 # check your schema and make sure to create students with all the necessary data
+Student.create(
+  name: "Christine",
+  age: 28,
+  cohort_id: c2.id
+)
+
+Student.create(
+  name: "Allie",
+  age: 26,
+  cohort_id: c2.id
+)
+
+Student.create(
+  name: "Ben",
+  age: 24,
+  cohort_id: c1.id
+)
+
+Student.create(
+  name: "Josh",
+  age: 19,
+  cohort_id: c3.id
+)
+
+Student.create(
+  name: "Mary",
+  age: 19,
+  cohort_id: c4.id
+)
 
 puts "Done!"
